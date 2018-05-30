4909152837bbe63bd44940beee7fc45aeb77dfa8
Raghu Betina <raghu@firstdraft.com>
2018-05-30 17:46:50 +0000

    Indirect association between users and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -14,6 +14,10 @@ class Restaurant < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :diners,
+             :through => :bookmarks,
+             :source => :user
+
   # Validations
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,10 @@ class User < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :favorites,
+             :through => :bookmarks,
+             :source => :restaurant
+
   # Validations
 
   # Include default devise modules. Others available are:
